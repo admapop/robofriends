181acbdc0899b72cbfcef04bf9ae26a81d4fd62d
admapop <52109936+admapop@users.noreply.github.com>
2019-07-06 21:35:04 +0200

    Initial functionality for Searchbox

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React from 'react'; //can also be written as React, { Component }
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-function App() {
-    return(
-        <div>
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={robots} />
-        </div>
-    );
+class App extends React.Component { //if declared as above, use just Component
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchField: ''
+        }
+    }
+    //when creating my own methods use arrow functions. IMPORTANT
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value }) //sets the searchfield to what I'm typing
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        })
+        console.log(filteredRobots); //gives actual value typed in the box
+    }
+
+    render() {
+        return(
+            <div className='tc' >
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={this.state.robots} />
+            </div>
+        );
+    }
 }
+
+
+//Simple function to render our page, it does note have state
+// function App() {
+//     return(
+//         <div className='tc' >
+//             <h1>RoboFriends</h1>
+//             <SearchBox />
+//             <CardList robots={robots} />
+//         </div>
+//     );
+// }
 
 export default App;
